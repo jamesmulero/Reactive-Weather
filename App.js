a0a5f3e59985e3cfd3da967809f5ba7c678f3a1e
jamesmulero <mulerojames@gmail.com>
2024-05-29 17:20:13 +0100

    Started the part 1 of this project. Exported Weather.js using module.exports. In the App.js i imported the cities array from data.js, the WeatherCard from /components/WeatherCard.js and rendered the City name, Temperature, Forecasted weather conditions

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import cities from "./data.js";
+import WeatherCard from "./components/WeatherCard.js";
 // Import data and WeatherCard here
 
-
 function App() {
+
     return (
         <>
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
                 {/* Render components here */}
-                
+                {cities.map((city) => <WeatherCard city={city} /> )};             
 
 
             </div>
